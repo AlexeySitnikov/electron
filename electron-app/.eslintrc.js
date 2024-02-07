030636e332d2e0d134dcf0a481fcb0c41e727597
Sitnikov <sitnikov@Mac-mini-Sitnikov.local>
2024-02-07 22:55:36 +0300

    first commit

---
+++ electron-app/.eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    semi: ['error', 'never'],
+    'react/jsx-uses-react': 0,
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-props-no-spreading': 0,
+    'react/prop-types': 0,
+    'no-console': 0,
+    'import/no-cycle': 0,
+    'import/prefer-default-export': 0,
+    'react/jsx-filename-extension': 0,
+    'linebreak-style': 0,
+    'import/no-extraneous-dependencies': 0,
+    'global-require': 0,
+  },
+}
