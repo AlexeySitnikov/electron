ef659dd158a03283d89bb1064077188c5b1455fc
Sitnikov <sitnikov@Mac-mini-Sitnikov.local>
2024-04-01 21:39:47 +0300

    trying to refactoring analizedBorders function

--- react-app/src/constrains/analizeBorders.js
+++ react-app/src/constrains/analizeBorders.js
@@ -4,12 +4,20 @@
 import { getLinesByNumber } from './getLinesByNumber'
 
 export async function analizeBorders(selectedFiles) {
-  const a = await Promise.all([...Array.from(selectedFiles).map((file, index) => {
-    if ((index === 0) || (index === Array.from(selectedFiles).length - 1)) {
-      return (getLinesByNumber(file))
-    }
-    return []
-  }),
+  const a = await Promise.all([...Array.from(selectedFiles)
+    .filter((_, index) => ((index === 0) || (index === Array.from(selectedFiles).length - 1)))
+    .forEach((file) => (getLinesByNumber(file))),
   ])
-  return (a[0].result[0].value.trim().replace(/\s\s+/g, ' ').split(' ')[2])
+  console.log(a[0].result[0].value.trim().replace(/\s\s+/g, ' ').split(' ')[2])
+  const fullZ = a.forEach((element) => element.result[0].value.trim().replace(/\s\s+/g, ' ').split(' ')[2])
+  return (fullZ[fullZ.length - 1] - fullZ[0])
 }
+
+// const a = await Promise.all([...Array.from(selectedFiles).map((file, index) => {
+//   if ((index === 0) || (index === Array.from(selectedFiles).length - 1)) {
+//     return (getLinesByNumber(file))
+//   }
+//   return []
+// }),
+// ])
+// return (a[0].result[0].value.trim().replace(/\s\s+/g, ' ').split(' ')[2])
