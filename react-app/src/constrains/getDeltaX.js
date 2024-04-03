d0f4b54a4b50b6e05b5134dc6fd752a7221ec5e9
Sitnikov <sitnikov@Mac-mini-Sitnikov.local>
2024-04-03 19:52:55 +0300

    add functionallity to find deltas of XYZ

--- react-app/src/constrains/getDeltaX.js
+++ react-app/src/constrains/getDeltaX.js
@@ -5,30 +5,26 @@ export function getDeltaX(file) {
   let deltaX = 0
   let previousValue = 0
   let currentValue = 0
-  //   let readNextLine = true
+
   return (
     new Promise(() => {
       const reader = new TxtReader()
-      reader.loadFile(file).progress((progress) => { console.log(progress) })
+      reader.loadFile(file)
         .then(() => {
           reader.getLines(startLine, 1).then((r) => {
-            previousValue = Math.abs(Number(r.result[0].trim().replace(/\s\s+/g, ' ').split(' ')[1]))
+            previousValue = Math.abs(Number(r.result[0].trim().replace(/\s\s+/g, ' ').split(' ')[2]))
           })
-            .then(() => {
+            .then(async () => {
               startLine += 1
               do {
-                // eslint-disable-next-line no-loop-func
-                reader.getLines(startLine, 1).then((r) => {
-                  currentValue = Math.abs(Number(r.result[0].trim().replace(/\s\s+/g, ' ').split(' ')[1]))
-                  if (previousValue !== currentValue) {
-                    deltaX = Math.abs(previousValue - currentValue)
-                    // readNextLine = false
-                  }
-                  console.log(deltaX)
-                  console.log(!Math.abs(previousValue - currentValue))
-                })
+                // eslint-disable-next-line no-await-in-loop
+                const r = await reader.getLines(startLine, 1)
+                currentValue = Math.abs(Number(r.result[0].trim().replace(/\s\s+/g, ' ').split(' ')[2]))
+                if (previousValue !== currentValue) {
+                  deltaX = Math.abs(previousValue - currentValue)
+                }
                 startLine += 1
-              } while (!Math.abs(previousValue - currentValue))
+              } while (deltaX === 0)
             })
         })
     })
