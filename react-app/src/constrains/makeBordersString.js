2537b660164a8dc82f984008baa4db2fceb63163
AlexeySitnikov <alexey.sitnikov@bk.ru>
2024-04-09 13:49:27 +0300

    small fix at deltasXYZ

--- react-app/src/constrains/makeBordersString.js
+++ react-app/src/constrains/makeBordersString.js
@@ -1,6 +1,6 @@
 export function makeBordersString({ setAddInformation, borders }) {
-  setAddInformation(`${String(borders.Zmax - borders.Zmin)} ${String((borders.Zmax - borders.Zmin) / 1000)}
-${String(borders.Xmax - borders.Xmin)} ${String(borders.Xmin / 1000)} ${String(borders.Xmax / 1000)}
-${String(borders.Ymax - borders.Ymin)} ${String(borders.Ymin / 1000)} ${String(borders.Ymax / 1000)}
+  setAddInformation(`${String((borders.Zmax - borders.Zmin) / borders.deltaZ)} ${String((borders.Zmax - borders.Zmin) / 1000)}
+${String((borders.Xmax - borders.Xmin) / borders.deltaX)} ${String(borders.Xmin / 1000)} ${String(borders.Xmax / 1000)}
+${String((borders.Ymax - borders.Ymin) / borders.deltaY)} ${String(borders.Ymin / 1000)} ${String(borders.Ymax / 1000)}
 1\n`)
 }
